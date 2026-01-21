b80085467afc1a99f6ce95df96f0a12dbb47cc1c
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-01-21 11:06:37 +0530

    Create Construct the Minimum Bitwise Array II.cpp

---
+++ Construct the Minimum Bitwise Array II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  vector<int> minBitwiseArray(vector<int>& A) {
+    vector<int> ans;
+    for (int x : A) {
+        if (x % 2 == 0) {
+            ans.push_back(-1);
+            continue;
+        }
+        int y = x + 1;
+        int bit = y & (-y);
+        ans.push_back(x - bit / 2);
+    }
+    return ans;
+}
+};
