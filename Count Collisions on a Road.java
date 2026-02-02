bcfea5af3a2c3f156c40028a0cb0c0df73429d26
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-02 16:11:23 +0530

    Create Count Collisions on a Road.java

---
+++ Count Collisions on a Road.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countCollisions(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j && s.charAt(i) == 'L')
+        {
+             i++;
+        } 
+        while (i <= j && s.charAt(j) == 'R')
+        { 
+            j--;
+        }
+        int collisions = 0;
+        for (int k = i; k <= j; k++) {
+            if (s.charAt(k) != 'S'){
+                collisions++;
+            } 
+        }
+        return collisions;
+    }
+}
