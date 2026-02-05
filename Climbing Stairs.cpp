10e80bc15f2a4f6db6861eeac8bac9e485af9480
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-05 13:02:00 +0530

    Fix indentation and formatting in climbStairs function

--- Climbing Stairs.cpp
+++ Climbing Stairs.cpp
@@ -3,7 +3,7 @@ public:
     int climbStairs(int n) {
         int a = 0, b = 1;
         for (int i = 0; i < n; ++i) {
-            int c = a + b;
+            int c = a + b; 
             a = b;
             b = c;
         }  
