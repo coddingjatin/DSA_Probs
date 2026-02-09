85e9ff8c2685232565a6f3b3d5236e8c3dd780da
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-09 09:16:48 +0530

    Added solution for 'Way Too Long Words' problem (That handles n number of strings)

---
+++ A. Way Too Long Words.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (n--) {
+        string s;
+        cin >> s;
+
+        if (s.size() <= 10) {
+            cout << s << endl;
+        } else {
+            cout << s[0] << s.size() - 2 << s.back() << endl;
+        }
+    }
+
+    return 0;
+}
