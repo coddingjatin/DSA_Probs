9715491db31b7e6f1d35a1273bfa489099a02c47
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-12 12:01:16 +0530

    Added Double Rent Problem
    Updated comments to clarify the problem statement and constraints.

---
+++ .cpp
@@ -0,0 +1,39 @@
+/* Double Rent
+Chefina decided to move into Chef's apartment.
+Chef was initially paying a rent of 
+X
+X rupees. Since Chefina is moving in, the owner decided to double the rent.
+
+Find the final rent Chef needs to pay.
+
+Input Format
+The input consists of a single integer 
+X
+X, denoting the rent Chef was initially paying.
+
+Output Format
+Output on a new line, the final rent Chef needs to pay.
+
+Eg: old rent 2, new rent will be 4.
+
+Constraints
+1
+≤
+X
+≤
+10
+1≤X≤10 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int X;
+cin>>X;
+if(X>=1 && X<=10){
+ int new_rent=2*X;
+ cout<<new_rent;
+   
+}
+
+}
