11f2a67fbf4ef848ea4b02dbe3438a2a36894c87
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-14 07:33:29 +0530

    Add program to find the maximum of two numbers

---
+++ Best of Two.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+    int X,Y;
+	while(T--){
+	    cin>>X>>Y;
+	    if(X>=Y){
+	        cout<<X<<endl;
+	    }else{
+	        cout<<Y<<endl;
+	    }
+	}
+
+}
