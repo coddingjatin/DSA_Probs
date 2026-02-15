1e69bded512940ad255ad84025018da9f0066ced
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-15 08:31:10 +0530

    Add Chef On Date program to evaluate conditions

---
+++ Chef On Date.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	int X,Y;
+	while(T--){
+	    cin>>X>>Y;
+	    if(X>=Y){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
