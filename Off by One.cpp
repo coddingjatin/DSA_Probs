26a08a82a70a6ea569d245f8c85774496c7a85a4
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-18 08:12:12 +0530

    Create Off by One.cpp

---
+++ Off by One.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int A, B;
+int res;
+cin>>A>>B;
+if(A>=1 && A<=50){
+    if(B>=1 && B<=50){
+        res=A+B;
+    }
+}
+cout<<res<<"1";
+}
