9d9f12598b63d92d04b152cfb393b53ebeed4eeb
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-19 10:02:32 +0530

    Created 116A - Tram.cpp

---
+++ 116A - Tram.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int a, b;
+    int current = 0;
+    int capacity = 0;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        current -= a;
+        current += b;
+        if (current > capacity) {
+            capacity = current;
+        }
+    }
+    
+    cout << capacity;
+    
+    return 0;
+}
