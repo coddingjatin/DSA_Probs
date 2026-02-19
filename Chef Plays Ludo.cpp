409120b9eaf144d6008518daa5d4565046156f81
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-19 09:20:52 +0530

    Created Chef Plays Ludo.cpp

---
+++ Chef Plays Ludo.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T,X;
+	cin>>T;
+	while(T--){
+	cin>>X;
+	if(X==6){
+	    cout<<"YES"<<endl;
+	}else{
+	    cout<<"NO"<<endl;
+	}
+	}
+
+}
