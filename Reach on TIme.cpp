106f7b34518138d7d065a53da3396743b5b44342
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-19 09:33:45 +0530

    Create Reach on TIme.cpp

---
+++ Reach on TIme.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int T,X;
+cin>>T;
+while(T--){
+    cin>>X;
+    if(X>=30){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+}
+}
