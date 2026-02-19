5137c3eab62e2e82938936f21ec58abc8204dd8a
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-19 09:57:14 +0530

    Created 344A - Magnets.cpp

---
+++ 344A - Magnets.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    string prev, curr;
+    int groups = 0;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> curr;
+        if (i == 0 || curr != prev) {
+            groups++;
+        }
+        prev = curr;
+    }
+    
+    cout << groups;
+    
+    return 0;
+}
