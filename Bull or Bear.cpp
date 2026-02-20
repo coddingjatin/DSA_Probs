b1fb14f00fec4984905a063302d0dfd1f36fc099
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-20 23:10:30 +0530

    Create Bull or Bear.cpp

---
+++ Bull or Bear.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        if (Y > X) {
+            cout << "PROFIT" << endl;
+        } 
+        else if (Y < X) {
+            cout << "LOSS" << endl;
+        } 
+        else {
+            cout << "NEUTRAL" << endl;
+        }
+    }
+    return 0;
+}
