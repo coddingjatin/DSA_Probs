c0099e66241e1fe9a68df5166ae12d1511dd6f74
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-23 08:41:07 +0530

    Create 2191A - Array Coloring.cpp

---
+++ 2191A - Array Coloring.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int odd = 0, even = 0;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            if (x % 2 == 0) even++;
+            else odd++;
+        }
+        if (abs(odd - even) <= 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
