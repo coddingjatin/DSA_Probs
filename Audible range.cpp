e555fb34e48be0a4f05ce4319930bd556fed8eeb
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-24 09:08:59 +0530

    Created Audible range.cpp

---
+++ Audible range.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T, X;
+	cin>>T;
+	while(T--){
+	    cin>>X;
+	    if(X>=67 && X<=45000){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+}
