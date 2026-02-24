6013ca61963e184f395fd281ef5e867e2b591598
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-24 09:16:33 +0530

    Created 427A - Police Recruits.cpp

---
+++ 427A - Police Recruits.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int available = 0;
+    int untreated = 0;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        if (x == -1) {
+            if (available > 0) {
+                available--;
+            } else {
+                untreated++;
+            }
+        } else {
+            available += x;
+        }
+    }
+
+    cout << untreated;
+
+    return 0;
+}
