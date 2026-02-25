8026231fdc654afe13d755a4b1594b76763ae09d
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-25 08:48:04 +0530

    Created Roller Coaster.cpp

---
+++ Roller Coaster.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T,X,H;
+	cin>>T;
+	while(T--){
+	    cin>>X>>H;
+	    if(X>=H){
+	        cout<<"YES"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+}
