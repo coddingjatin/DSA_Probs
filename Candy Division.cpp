445e198098338f0f9ccd284e7c1cc579896a6d74
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-27 01:58:19 +0530

    Created Candy Division.cpp

---
+++ Candy Division.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        if (N % 3 == 0)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
