f08d1fd105a26c4600688451106ccf7d1fa16dcf
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-02-28 11:02:26 +0530

    Created Right There.cpp

---
+++ Right There.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+
+        if (N <= X)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
